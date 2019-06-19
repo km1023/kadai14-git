159596ad1465ec141a88bc33c205405e8970bf3d
km1023 <km.rooks.312@gmail.com>
2019-06-20 08:18:26 +0900

    Add Employee create

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -8,36 +8,36 @@ import javax.persistence.EntityManager;
 import models.Employee;
 import utils.DBUtil;
 
-public class EmployeeVlidator {
-    
+public class EmployeeValidator {
+
     public static List<String> validate(Employee e,Boolean code_duplicate_check_flag, Boolean password_check_flag) {
         List<String> errors = new ArrayList<String>();
-        
+
         String code_error = _validateCode(e.getCode(), code_duplicate_check_flag);
         if(!code_error.contentEquals("")) {
             errors.add(code_error);
         }
-        
+
         String name_error = _validateName(e.getName());
         if(!name_error.equals("")) {
             errors.add(name_error);
         }
-        
+
         String password_error = _validatePassword(e.getPassword(), password_check_flag);
         if(!password_error.equals("")) {
             errors.add(password_error);
         }
-        
+
         return errors;
     }
-    
+
     //社員番号
     private static String _validateCode(String code, Boolean code_duplicate_check_flag) {
         //必須入力チェック
         if(code == null || code.contentEquals("")) {
             return "社員番号を入力してください";
         }
-        
+
         //既に登録されている社員番号との重複チェック
         if(code_duplicate_check_flag) {
             EntityManager em = DBUtil.createEntityManager();
@@ -51,23 +51,23 @@ public class EmployeeVlidator {
         }
         return "";
     }
-    
+
     //社員名の必須入力チェック
     private static String _validateName(String name) {
         if(name == null || name.equals("")) {
             return "氏名を入力してください";
         }
-        
+
         return "";
     }
-    
+
     //パスワードの必須入力チェック
     private static String _validatePassword(String password, Boolean password_check_flag) {
         //パスワードを変更する場合のみ実行
         if(password_check_flag && (password == null || password.contentEquals(""))) {
             return "パスワードを入力してください";
         }
-        
+
         return "";
     }
 }
